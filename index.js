a42a0f54a99eef77c0e8b7dd7b3ee43a04cf9af4
kaywanbalbas <kaywanbalbas@gmail.com>
2020-06-03 19:07:30 -0700

    Add functionality to play on mouse click

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-alert("Press OK to roll!");
-// generate random integer from 1 to 6
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1
+// listens for mouse click to play game
+document.addEventListener("click", function(event) {
+  play();
+});
 
-// set randomImage for dice based on randomNumber
-var randomImage1 = "images/dice" + randomNumber1 + ".png"
-var randomImage2 = "images/dice" + randomNumber2 + ".png"
+function play() {
+  // generate random integer from 1 to 6
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1
 
-// changes src attribute for img1 and img2 based on randomImage for dice
-document.querySelector("img.img1").setAttribute("src", randomImage1);
-document.querySelector("img.img2").setAttribute("src", randomImage2);
+  // set randomImage for dice based on randomNumber
+  var randomImage1 = "images/dice" + randomNumber1 + ".png"
+  var randomImage2 = "images/dice" + randomNumber2 + ".png"
 
-var winner = "";
-// checks for winner
-if (randomNumber1 > randomNumber2) {
-  winner = "🚩 Player 1 Wins!"
+  // changes src attribute for img1 and img2 based on randomImage for dice
+  document.querySelector("img.img1").setAttribute("src", randomImage1);
+  document.querySelector("img.img2").setAttribute("src", randomImage2);
+
+  var winner = "";
+  // checks for winner
+  if (randomNumber1 > randomNumber2) {
+    winner = "🚩 Player 1 Wins!"
+  } else if (randomNumber2 > randomNumber1) {
+    winner = "Player 2 Wins! 🚩"
+  } else {
+    winner = "😱 Draw! 😱"
+  }
+
+  // changes innerHTML value of h1 to display the winner
+  document.querySelector("h1").innerHTML = winner;
+
 }
-else if (randomNumber2 > randomNumber1) {
-  winner = "Player 2 Wins! 🚩"
-}
-else {
-  winner = "😱 Draw! 😱"
-}
-
-// changes innerHTML value of h1 to display the winner
-document.querySelector("h1").innerHTML = winner;
